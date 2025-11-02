ba02bf0e41b777d33809b012206f4cb9cf86aa87
Mithleshk595 <mithleshk0982@gmail.com>
2025-11-02 21:28:33 +0530

    First commit

---
+++ Data_test.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+data = {
+    'Name': ['Amit', 'Riya', 'John'],
+    'Age': [25, 22, 28],
+    'City': ['Delhi', 'Mumbai', 'Kolkata']
+}
+
+df = pd.DataFrame(data)
+print(df)
